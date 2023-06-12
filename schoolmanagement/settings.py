8e2a6087aab8d60e0288fe27744e2a77fd0ec3a5
InyangUkpong <inyangweb@gmail.com>
2023-06-12 23:16:38 +0100

    new commit

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'k0ujs9pcw+7qohwas!o7_ept20$c@$)-b=qco8sgviy_f)((bc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'schoolmanagement.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": 'verceldb',
+        "USER": 'default',
+        "PASSWORD": 'no4EWSmzIb1u',
+        "HOST": 'ep-gentle-scene-697228-pooler.us-east-1.postgres.vercel-storage.com',
+        'PORT': '5432',
+    
+    },
 }
 
 
@@ -115,7 +120,8 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
+
 
 
 # Static files (CSS, JavaScript, Images)
