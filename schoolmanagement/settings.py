cc3305d43bb11b995b4f970b912560e8c94e1494
InyangUkpong <inyangweb@gmail.com>
2023-06-13 00:05:43 +0100

    edited settings staticfiles to static on line 132 and 136

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -127,12 +127,13 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+os.path.join(BASE_DIR, 'media')
+os.path.join(BASE_DIR, 'static')
 
 
 
